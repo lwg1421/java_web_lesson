56294d784e5aae03cb87f6368134f237a80b73e4
A <A@DESKTOP-NKSGQVR>
2023-01-16 18:10:03 +0900

    사용자 입력값 DB에 insert

---
+++ web/src/main/java/com/oraclejava/Insert.java
@@ -0,0 +1,84 @@
+package com.oraclejava;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Insert")
+
+public class Insert extends HttpServlet {
+	
+	@Override
+	// 페이지를 새로고침하면 이부분이 실행되는 것임
+	// goGet이라는 이 부분이 화면에 나오는 것임!
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 이걸 작성해줘야 한글 안깨짐
+		resp.setContentType("text/html; charset=utf-8");
+		// 여기에 작성한 내용이 화면에 출력됨
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html><head><title>JDBC Insert 예제</title></head><body>");
+		out.println("<h1> 이름을 입력하세요 </h1>");
+		out.println("<form name =\"myform\" action =\"/web/Insert\" method = \"post\">");
+		out.println("이름 : <input type =\"text\" name=\"name\"><br>");
+		out.println("<input type=\"submit\" value=\"저장\">");
+		out.println("</form></body></html>");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		// 한글 안깨지게..
+        resp.setContentType("text/html; charset = utf-8");
+        req.setCharacterEncoding("utf-8");
+        
+        try {
+			// 오라클 드라이버를 Load 한다
+			Class.forName("oracle.jdbc.OracleDriver");
+
+			// 데이타 베이스에 접속을 한다.
+			// 오라클에서 확인해서 @호스트이름 포트 서비스이름 에 넣어주면 됨.
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/oraclejava", "hr", "hr");
+			con.setAutoCommit(false);
+			
+			String name = req.getParameter("name");
+			
+			stmt = con.createStatement();
+			
+			String query = "insert into employees(employee_id, last_name, email, hire_date, job_id) values ("
+			+ "employees_seq.nextval,'" + name + "','test@naver.com',sysdate, 'IT_PROG')";
+			
+			stmt.executeQuery(query);
+			con.commit();
+			stmt.close();
+			
+			resp.sendRedirect("/web/Insert");
+        } catch (ClassNotFoundException e) {
+			System.out.println("Couldn't load database driver: " + e.getMessage());
+		} catch (SQLException e) {
+			System.out.println("SQLException caught: " + e.getMessage());
+		} finally {
+			// 언제나 데이타 베이스 연결을 종료한다.
+			try {
+				if (con != null)
+					con.close();
+			} catch (SQLException ignored) {
+			}
+		}
+        
+        
+	}
+}
---
+++ web/src/main/java/com/oraclejava/MovieInsert.java
@@ -0,0 +1,93 @@
+package com.oraclejava;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MovieInsert")
+public class MovieInsert extends HttpServlet {
+	
+	@Override
+	// 페이지를 새로고침하면 이부분이 실행되는 것임
+	// goGet이라는 이 부분이 화면에 나오는 것임!
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 이걸 작성해줘야 한글 안깨짐
+		resp.setContentType("text/html; charset=utf-8");
+		// 여기에 작성한 내용이 화면에 출력됨
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html><head><title>JDBC Insert 예제</title></head><body>");
+		
+		out.println("<form name =\"myform\" action =\"/web/MovieInsert\" method = \"post\">");
+		out.println("<h3>영화 정보를 입력하세요</h3>");
+		out.println("영화ID : <input type =\"text\" name=\"movie_id\"><br>");
+		out.println("영화이름 : <input type =\"text\" name=\"movie_name\"><br>");
+		out.println("상영시간 : <input type =\"text\" name=\"movie_time\"><br>");
+		out.println("제작스튜디오 : <input type =\"text\" name=\"movie_studio\"><br>");
+		out.println("출시년도 : <input type =\"text\" name=\"movie_year\"><br>");
+		
+		out.println("<input type=\"submit\" value=\"저장\">");
+		out.println("</form></body></html>");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		// 한글 안깨지게..
+        resp.setContentType("text/html; charset = utf-8");
+        req.setCharacterEncoding("utf-8");
+        
+        try {
+			// 오라클 드라이버를 Load 한다
+			Class.forName("oracle.jdbc.OracleDriver");
+
+			// 데이타 베이스에 접속을 한다.
+			// 오라클에서 확인해서 @호스트이름 포트 서비스이름 에 넣어주면 됨.
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/oraclejava", "movie", "movie");
+			con.setAutoCommit(false);
+			
+			String movie_id = req.getParameter("movie_id");
+			String movie_name = req.getParameter("movie_name");
+			int movie_time = Integer.parseInt(req.getParameter("movie_time"));
+			String movie_studio = req.getParameter("movie_studio");
+			int movie_year = Integer.parseInt(req.getParameter("movie_year"));
+			
+			stmt = con.createStatement();
+			
+			// 해킹에 취약함
+			String query = "insert into movie(movie_id, movie_name, running_time, studio, year_of_release) values('"+movie_id+"','"+movie_name+"',"+movie_time+",'"+movie_studio+"',"+movie_year+")";
+			
+			stmt.executeQuery(query);
+			con.commit();
+			stmt.close();
+			
+			resp.sendRedirect("/web/MovieInsert");
+        } catch (ClassNotFoundException e) {
+			System.out.println("Couldn't load database driver: " + e.getMessage());
+		} catch (SQLException e) {
+			System.out.println("SQLException caught: " + e.getMessage());
+		} finally {
+			// 언제나 데이타 베이스 연결을 종료한다.
+			try {
+				if (con != null)
+					con.close();
+			} catch (SQLException ignored) {
+			}
+		}
+        
+        
+	}
+}
